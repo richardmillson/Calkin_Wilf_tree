d2bbe9690afa15ee57e6761688ce0a1a7f71ec45
richardmillson <8217613+richardmillson@users.noreply.github.com>
2016-06-05 10:05:05 -0400

    Improve get_nth

--- tree.py
+++ tree.py
@@ -3,7 +3,7 @@ from fractions import Fraction
 import itertools
 
 
-def succ_fcn(x):
+def succ(x):
     """
     takes an element of the Calkin Wilf tree and returns the next element
     following a breadth first traversal
@@ -13,25 +13,20 @@ def succ_fcn(x):
     return Fraction(1 / (x_int + 1 - x_nonint))
 
 
-def generator_succ():
+def entire_tree():
     """
     a generator for the entire Calkin Wilf tree
     """
     x = Fraction(1, 1)
     yield x
     while True:
-        x_int = Fraction(math.floor(x))
-        x_nonint = Fraction(x.numerator - x_int * x.denominator, x.denominator)
-        x = Fraction(1 / (x_int + 1 - x_nonint))
+        x = succ(x)
         yield x
 
 
 def get_nth(n):
     """
-    takes a natural number n and returns the nth element of the Calkin Wilf tree
+    takes a positive integer n and returns the nth element of the Calkin Wilf tree
     following a breadth first traversal
     """
-    node = Fraction(1, 1)
-    for i in range(1, n + 1):
-        node = succ_fcn(node)
-    return node
+    return list(itertools.islice(entire_tree(), n - 1, n))[0]
--- tree_test.py
+++ tree_test.py
@@ -1,22 +1,25 @@
 from tree import *
 
+first_7 = [Fraction(1, 1), Fraction(1, 2), Fraction(2, 1), Fraction(1, 3), Fraction(3, 2), Fraction(2, 3), Fraction(3, 1)]
 
-def test_succ_fcn():
-    first_7 = [Fraction(1, 1), Fraction(1, 2), Fraction(2, 1), Fraction(1, 3), Fraction(3, 2), Fraction(2, 3),
-               Fraction(3, 1)]
 
+def test_succ():
     gen_7 = [Fraction(1, 1)]
     for n in range(2, 8):
-        gen_7 += [succ_fcn(gen_7[n - 2])]
-
+        gen_7 += [succ(gen_7[n - 2])]
     assert first_7 == gen_7
 
-def test_generator_succ():
-    start = 0
-    end = 7
-    gen_7 = list(itertools.islice(generator_succ(), start, end))
-    print gen_7
-    print len(gen_7)
 
-test_succ_fcn()
-test_generator_succ()
+def test_entire_tree():
+    start = 0
+    stop = 7
+    gen_7 = list(itertools.islice(entire_tree(), start, stop))
+    assert first_7 == gen_7
+
+
+def test_get_nth():
+    assert first_7[6] == get_nth(7)
+
+test_succ()
+test_entire_tree()
+test_get_nth()
