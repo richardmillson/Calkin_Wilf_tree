e8c39e64ba8a6c7835d19200e26f9e2988225a01
richardmillson <8217613+richardmillson@users.noreply.github.com>
2016-06-05 15:12:50 -0400

    Add timer to compare get_nth approaches

--- tree.py
+++ tree.py
@@ -7,6 +7,8 @@ def succ(x):
     """
     takes an element of the Calkin Wilf tree and returns the next element
     following a breadth first traversal
+    :param x: Fraction
+    :return: Fraction
     """
     x_int = Fraction(math.floor(x))
     x_nonint = Fraction(x.numerator - x_int * x.denominator, x.denominator)
@@ -27,19 +29,23 @@ def entire_tree():
 
 def get_nth(n):
     """
-    takes a positive integer n and returns the nth element of the Calkin Wilf tree
-    following a breadth first traversal
-    :param n: int
+    returns the nth element of the tree following a breadth first traversal
+    :param n: positive int
     :return: Fraction
     """
-    return get_slice(n - 1, n)[0]
+    x = Fraction(1, 1)
+    i = 0
+    while i < n:
+        x = succ(x)
+        i += 1
+    return x
 
 
 def get_slice(start, stop):
     """
     return a finite sublist from the infinite generator
-    :param start: int
-    :param stop: int
-    :return: list
+    :param start: positive int
+    :param stop: positive int greater than start
+    :return: tuple
     """
     return tuple(itertools.islice(entire_tree(), start, stop))
--- tree_test.py
+++ tree_test.py
@@ -1,4 +1,5 @@
 from tree import *
+import timeit
 
 given_first_7 = (Fraction(1, 1), Fraction(1, 2), Fraction(2, 1), Fraction(1, 3), Fraction(3, 2), Fraction(2, 3), Fraction(3, 1))
 
@@ -16,9 +17,40 @@ def test_entire_tree():
 
 
 def test_get_nth():
-    assert get_nth(7) == given_first_7[6]
+    assert get_nth(3) == given_first_7[3]
+    assert get_nth(6) == given_first_7[6]
+
+
+def time_get_nth(n):
+    """
+    time how long it takes the accumulator to get the nth element
+    :param n: positive int
+    :return: float
+    """
+    start = timeit.default_timer()
+    get_nth(n)
+    stop = timeit.default_timer() - start
+    return stop
+
+
+def time_get_slice(n):
+    """
+    time how long it takes get_slice to get the nth element
+    :param n: positive int
+    :return: float
+    """
+    start = timeit.default_timer()
+    get_slice(n-1, n)
+    stop = timeit.default_timer() - start
+    return stop
 
 
 test_succ()
 test_entire_tree()
 test_get_nth()
+
+print given_first_7
+n = 2
+print time_get_nth(n), get_nth(n)
+print time_get_slice(n), get_slice(n, n + 1)[0]
+# print time_get_nth(100000)
